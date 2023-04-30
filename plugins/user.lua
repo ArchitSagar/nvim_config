183da46563c28de069be8921cee43b41942fe2a5
Piyush Singh <singhpiyush998@gmail.com>
2023-04-30 22:18:27 +0530

    Move: rosepine config into its separate file

--- plugins/user.lua
+++ plugins/user.lua
@@ -1,30 +1,20 @@
 return {
-  -- You can also add new plugins here as well:
-  -- Add plugins, the lazy syntax
-  -- "andweeb/presence.nvim",
-  -- {
-  --   "ray-x/lsp_signature.nvim",
-  --   event = "BufRead",
-  --   config = function()
-  --     require("lsp_signature").setup()
-  --   end,
-  -- },
-  {
-    "rose-pine/neovim",
-    name = "rose-pine",
-    config = function()
-      require("rose-pine").setup {
-        disable_background = true,
-        disable_float_background = true,
-      }
-    end,
-  },
-  {
-    "rcarriga/nvim-notify",
-    config = function()
-      require("notify").setup {
-        background_colour = "#000000",
-      }
-    end,
-  },
+    -- You can also add new plugins here as well:
+    -- Add plugins, the lazy syntax
+    -- "andweeb/presence.nvim",
+    -- {
+    --   "ray-x/lsp_signature.nvim",
+    --   event = "BufRead",
+    --   config = function()
+    --     require("lsp_signature").setup()
+    --   end,
+    -- },
+    {
+        "rcarriga/nvim-notify",
+        config = function()
+            require("notify").setup {
+                background_colour = "#000000",
+            }
+        end,
+    },
 }
