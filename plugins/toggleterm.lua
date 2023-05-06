50b3590d5695f27651cb030bfea06e21a773c5f4
Piyush Singh <singhpiyush998@gmail.com>
2023-05-06 14:11:38 +0530

    Change: Keymap for 2nd terminal window

--- plugins/toggleterm.lua
+++ plugins/toggleterm.lua
@@ -1,7 +1,7 @@
 function _G.set_terminal_keymaps()
   local opts = { noremap = true }
   vim.keymap.set("t", "jk", [[<C-\><C-n>]], opts)
-  vim.keymap.set("t", "<C-t>", [[<Cmd>:2ToggleTerm<CR>]], opts)
+  vim.keymap.set("t", "<leader>tt", [[<Cmd>:2ToggleTerm<CR>]], opts)
 end
 
 vim.cmd "autocmd! TermOpen term://* lua set_terminal_keymaps()"
