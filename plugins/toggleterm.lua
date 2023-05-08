75e401541bbe3808ae38b99e33e3461abf75f14f
Piyush Singh <singhpiyush998@gmail.com>
2023-05-08 11:09:17 +0530

    Update: Toggleterm keybindings

--- plugins/toggleterm.lua
+++ plugins/toggleterm.lua
@@ -1,7 +1,8 @@
 function _G.set_terminal_keymaps()
   local opts = { noremap = true }
   vim.keymap.set("t", "jk", [[<C-\><C-n>]], opts)
-  vim.keymap.set("t", "<leader>tt", [[<Cmd>:2ToggleTerm<CR>]], opts)
+  vim.keymap.set("t", "<leader>tt", [[<Cmd>:2ToggleTerm direction=horizontal<CR>]], opts)
+  vim.keymap.set("t", "<leader>tT", [[<Cmd>:3ToggleTerm direction=horizontal<CR>]], opts)
 end
 
 vim.cmd "autocmd! TermOpen term://* lua set_terminal_keymaps()"
@@ -12,7 +13,7 @@ return {
     size = 10,
     open_mapping = [[<F7>]],
     shading_factor = 2,
-    direction = "horizontal",
+    direction = "float",
     float_opts = {
       border = "curved",
       highlights = { border = "Normal", background = "Normal" },
